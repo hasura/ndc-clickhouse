e177c7bac0309bfad417fba3ab84c47cbc7a4870
Benoit Ranque <ranque.benoit@gmail.com>
2024-03-19 22:19:39 -0400

    correct artifacts

--- ci/scripts/generate-connector-definition.sh
+++ ci/scripts/generate-connector-definition.sh
@@ -5,4 +5,4 @@ ROOT="$(pwd)"
 
 mkdir -p "${ROOT}/release/connector-definition/.hasura-connector/"
 cat "${ROOT}/ci/templates/connector-metadata.yaml" | envsubst > "${ROOT}/release/connector-definition/.hasura-connector/connector-metadata.yaml"
-tar -czvf "${ROOT}/release/connector-definition.tgz" "${ROOT}/release/connector-definition/"
+tar -czvf "${ROOT}/release/artifacts/connector-definition.tgz" "${ROOT}/release/connector-definition/"
--- ci/scripts/generate-manifest.sh
+++ ci/scripts/generate-manifest.sh
@@ -3,11 +3,11 @@
 set -evo pipefail
 ROOT="$(pwd)"
 
-export LINUX_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep ndc-clickhouse-cli-x86_64-unknown-linux-musl  | cut -f1 -d' ')
-export MACOS_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep ndc-clickhouse-cli-x86_64-apple-darwin        | cut -f1 -d' ')
-export WINDOWS_AMD64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum | grep ndc-clickhouse-cli-x86_64-pc-windows-msvc.exe | cut -f1 -d' ')
-export LINUX_ARM64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep ndc-clickhouse-cli-aarch64-unknown-linux-musl | cut -f1 -d' ')
-export MACOS_ARM64_SHA256=$(cat ${ROOT}/release/artifacts/sha256sum   | grep ndc-clickhouse-cli-aarch64-apple-darwin       | cut -f1 -d' ')
+export LINUX_AMD64_SHA256=$(sha256sum ${ROOT}/release/artifacts/ndc-clickhouse-cli-x86_64-unknown-linux-musl     | cut -f1 -d' ')
+export MACOS_AMD64_SHA256=$(sha256sum ${ROOT}/release/artifacts/ndc-clickhouse-cli-x86_64-apple-darwin           | cut -f1 -d' ')
+export WINDOWS_AMD64_SHA256=$(sha256sum ${ROOT}/release/artifacts/ndc-clickhouse-cli-x86_64-pc-windows-msvc.exe  | cut -f1 -d' ')
+export LINUX_ARM64_SHA256=$(sha256sum ${ROOT}/release/artifacts/ndc-clickhouse-cli-aarch64-unknown-linux-musl    | cut -f1 -d' ')
+export MACOS_ARM64_SHA256=$(sha256sum ${ROOT}/release/artifacts/ndc-clickhouse-cli-aarch64-apple-darwin          | cut -f1 -d' ')
 
 mkdir -p "${ROOT}/release/"
 cat "${ROOT}/ci/templates/manifest.yaml" | envsubst > "${ROOT}/release/manifest.yaml"
