0fa020d4cb673be344842eb37131969f1d3e022d
Benoit Ranque <ranque.benoit@gmail.com>
2024-03-19 22:45:16 -0400

    inline short CI scripts

--- ci/scripts/generate-connector-definition.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -evo pipefail
-ROOT="$(pwd)"
-
-mkdir -p "${ROOT}/release/connector-definition/.hasura-connector/"
-cat "${ROOT}/ci/templates/connector-metadata.yaml" | envsubst > "${ROOT}/release/connector-definition/.hasura-connector/connector-metadata.yaml"
-tar -czvf "${ROOT}/release/artifacts/connector-definition.tgz" "${ROOT}/release/connector-definition/"
