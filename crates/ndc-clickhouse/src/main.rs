3466ee36b620dd69981689c55269957304b81380
Benoit Ranque <ranque.benoit@gmail.com>
2024-07-10 06:19:48 -0400

    enable multitenant

--- crates/ndc-clickhouse/src/main.rs
+++ crates/ndc-clickhouse/src/main.rs
@@ -1,9 +1,9 @@
-use ndc_clickhouse::connector::ClickhouseConnector;
+use ndc_clickhouse::connector::setup::ClickhouseConnectorSetup;
 use ndc_sdk::default_main::default_main;
 
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    default_main::<ClickhouseConnector>().await
+    default_main::<ClickhouseConnectorSetup>().await
 }
